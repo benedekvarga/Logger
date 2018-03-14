9c32bab96514ab1dc523ce640f3fc60f115a3064
Benedek Varga <vargabenedek92@gmail.com>
2018-03-14 09:04:42 +0100

    Initial Commit

---
+++ Logger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Logger
+//
+//  Created by Benedek Varga on 03/14 W 73.
+//  Copyright © 2018. Benedek Varga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
